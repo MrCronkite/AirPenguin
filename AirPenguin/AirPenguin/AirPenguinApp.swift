1aa3b8ee6c51351717a46455d4e76060238caf99
Влад Шимченко <vladshima99@gmail.com>
2026-08-13 17:38:26 +0300

    ClearRepo

---
+++ AirPenguin/AirPenguin/AirPenguinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirPenguinApp.swift
+//  AirPenguin
+//
+//  Created by Влад Шимченко on 13.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct AirPenguinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
